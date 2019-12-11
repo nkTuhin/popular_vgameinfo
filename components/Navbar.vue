b62590963174c74d1cff3bc9fcd99d52db830625
nk <nktuhin12@gmail.com>
2019-12-11 17:36:35 +0600

    Navbar Component

---
+++ components/Navbar.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <ul class="flex">
+      <li class="mr-6">
+        <nuxt-link class="text-blue-500 hover:text-blue-800" to="/">Home</nuxt-link>
+        <nuxt-link class="text-blue-500 hover:text-blue-800" to="/about">About</nuxt-link>
+      </li>
+    </ul>
+  </div>
+</template>
