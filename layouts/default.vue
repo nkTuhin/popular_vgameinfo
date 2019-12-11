18b56e1b11e717de0d809b2a8b09d41e4cbeb8f5
nk <nktuhin12@gmail.com>
2019-12-11 17:37:18 +0600

    Nuxt videoGame info from API

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <Navbar />
+    <nuxt />
+  </div>
+</template>
+
+<script>
+import Navbar from '@/components/Navbar'
+import axios from 'axios'
+
+axios.defaults.headers.common['user-key'] = 'user_key'
+axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest'
+
+export default {
+  components: {
+    Navbar
+  }
+}
+</script>
+
+<style>
+</style>
