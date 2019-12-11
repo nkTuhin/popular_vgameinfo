815162f4b3129f6ca8a86a8cbbec9907ce887d65
nk <nktuhin12@gmail.com>
2019-12-11 17:36:19 +0600

    All popular gamei info

---
+++ pages/index.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="container mx-auto py-16">
+    <h1 class="font-heading uppercase mb-8">Popular Games</h1>
+    <div class="game-container flex flex-wrap -mx-4">
+      <nuxt-link
+        :to="'/games/' + game.id"
+        v-for="game in games"
+        :key="game.id"
+        class="w-full md:w-1/5 px-4 mb-12 no-underline"
+      >
+        <img :src="game.cover.url.replace('t_thumb', 't_cover_big')" alt="cover" />
+        <div
+          class="text-black font-semibold text-lg overflow-hidden whitespace-no-wrap overflow-dots"
+        >{{ game.name }}</div>
+        <div
+          class="text-grey-darker text-base overflow-hidden whitespace-no-wrap overflow-dots pb-1"
+        >{{ game.genres[0].name }}</div>
+      </nuxt-link>
+    </div>
+  </div>
+</template>
+<script>
+import axios from 'axios'
+
+export default {
+  asyncData({ params, error }) {
+    const proxyurl = 'https://cors-anywhere.herokuapp.com/'
+
+    return axios
+      .get(
+        `${proxyurl}https://api-v3.igdb.com/games/?fields=name,genres.name,cover.url,popularity&order=popularity:desc&expand=genres`
+      )
+      .then((res) => {
+        return {
+          games: res.data
+        }
+      })
+      .catch((e) => {
+        console.log(e)
+      })
+  },
+  data() {
+    return {}
+  }
+}
+</script>
+
+<style>
+</style>
